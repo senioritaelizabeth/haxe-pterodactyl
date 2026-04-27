312b6cdb8f103d8ce021239d85ddfad3245669ac
José Ortiz <me@mdcdev.me>
2026-04-27 12:08:42 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,6 @@ cd /home/container || exit 1
 echo "User: $(whoami)"
 echo "Dir: $(pwd)"
 echo "Haxe: $(haxe --version)"
-echo "Neko: $(neko -version || true)"
-
-if [ -z "${STARTUP}" ]; then
-  echo "STARTUP no definido"
-  exit 1
-fi
+echo "Haxelib ready"
 
 exec bash -lc "${STARTUP}"
