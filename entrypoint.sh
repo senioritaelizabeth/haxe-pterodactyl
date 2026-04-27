dd975be940bb548f3f0c55859ae3234d7632efe9
José Ortiz <me@mdcdev.me>
2026-04-27 11:29:16 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 cd /home/container || exit 1
-echo "Container ready"
-printf '%s\n' "$STARTUP"
-exec sh -c "$STARTUP"
+
+echo "Container starting..."
+echo "User: $(whoami)"
+echo "Working dir: $(pwd)"
+
+if [ -z "${STARTUP}" ]; then
+  echo "No STARTUP variable defined"
+  exit 1
+fi
+
+echo "Running startup command: ${STARTUP}"
+exec bash -c "${STARTUP}"
